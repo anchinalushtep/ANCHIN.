48027cc99253617489eab20d6b36c409042fbf9f
anchinalushtep <anchinalushtep@gmail.com>
2025-08-12 23:55:54 -0700

    Add files via upload

---
+++ Anchinalu.py
@@ -0,0 +1,6 @@
+num1 =float(input("enter the first number: "))
+num2 =float(input("enter the second number: "))
+num3 =float(input("enter the third number: "))
+
+largest =max(num1, num2, num3)
+print("the largest number is :", largest)
